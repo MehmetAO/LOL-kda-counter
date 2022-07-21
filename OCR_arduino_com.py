82af08daafd949012f51afecc72d6ac1f200a1ac
Mehmet Ali <109021192+MehmetAO@users.noreply.github.com>
2022-07-21 20:32:04 +0300

    Add files via upload

---
+++ OCR_arduino_com.py
@@ -0,0 +1,50 @@
+import numpy as nm
+import pytesseract
+import cv2
+import serial
+import time
+from PIL import ImageGrab
+
+arduinoData = serial.Serial("COM4", 9600)  # Arduino comm channel
+time.sleep(3)  # Waiting for the arduino
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Pytesseract location on PC
+
+death_count = 0  # Predetermined death count
+kill_count = 0  # Predetermined kill count
+kdaloc = (1663, 0, 1720, 25)  # K/D/A location on screen
+
+def ardupinOn():  # Sending 1 (True) to arduino
+    msg = "1"
+    arduinoData.write(msg.encode())
+
+
+def ardupinOff():  # Sending 0 (False) to arduino
+    a = "0"
+    arduinoData.write(a.encode())
+
+
+def imToString(kda):  # Turning kill/death/assist to data
+    cap = ImageGrab.grab(bbox=kda)
+    tesstr = pytesseract.image_to_string(
+        cv2.cvtColor(nm.array(cap),
+                     cv2.COLOR_BGR2GRAY),
+        config='--psm 10 --oem 3 -c tessedit_char_whitelist=/0123456789',  # Whitelist just numbers for K/D/A
+        lang='eng')
+
+    return tesstr
+
+while True:
+    data = imToString(kdaloc)
+    #print(data)
+    if data != "":
+        kill = data[0]
+        death = data[2]
+        if int(death) > int(death_count):
+            ardupinOn()
+            death_count = death
+
+        else:
+            ardupinOff()
+
+# 1663, 0, 1720, 25
